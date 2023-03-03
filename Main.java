b7d3e4bd3145142a23f5bd7d252b45f0ef1aba12
K. Max <113927586+KMaxKR@users.noreply.github.com>
2023-03-03 18:52:31 +0200

    ArrayList

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Max");
+
+
+    }
+}
